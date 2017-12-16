3e7ee18ee907476907c15f3aa7ea1a80e7136849
Luis Mayta <luis.mayta@gmail.com>
2017-12-15 19:17:41 -0500

    Funcional

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Drupal\importjson\Controller;
 use Drupal\Core\Controller\ControllerBase;
+use \Drupal\node\Entity\Node;
 /**
  * Importa JSON
  */
@@ -8,11 +9,37 @@ use Drupal\Core\Controller\ControllerBase;
 class MainController extends ControllerBase {
 
 	public function test() {
-		return array(
-			'#markup' => t('hola'),
-		);
+		$uri = "http://167.249.9.43:1880/RADA/ServicioExternoSIDARH.asmx/Normativa";
+		try {
+		    $response = \Drupal::httpClient()->get($uri, array('headers' => array('Accept' => 'text/plain')));
+		    $data = (string) $response->getBody();
+
+		  }
+		  catch (RequestException $e) {
+		    echo "error";
+		  }
+
+		  $datos = json_decode($data, true);
+
+		  foreach ($datos as $key => $item) {
+			// Create node object with attached file.
+			$node = Node::create([
+				'type' => 'article',
+				'title' => $item['Tit'],
+				'description' => $item['Mat'],
+			]);
+			$node->save();
+		  }
+
+
+		return [
+	      '#type' => 'markup',
+	      '#markup' =>'datos importados :'.count($datos)
+	    ];
 	}
 }
+
+
 
 
 
